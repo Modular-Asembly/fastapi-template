c647f20bcfd484b32505c33428aa9abfa4ba96e5
Luis Galeana <lsgaleana@gmail.com>
2025-01-04 13:06:49 -0600

    First commit

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+load_dotenv()
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
